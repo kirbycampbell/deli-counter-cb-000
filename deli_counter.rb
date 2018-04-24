9e12629af8b9af08348b4d949189298165126aab
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-24 20:00:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,22 @@
-def line(deli_line, name)
+def line(deli_line)
   if deli_line.length < 1
     puts "The line is currently empty."
   elsif deli_line.length > 0
       return deli_line
+      puts "The line is currently: #{deli_line[0]}"
   end
 end
 
 def take_a_number(deli_line, name)
-
+  deli_line << name
+  puts "Welcome, #{deli_line[-1]}. You are number #{deli_line.length} in line."
 end
 
-def now_serving
-
+def now_serving(deli_line, name)
+  if deli_line.length < 1
+    puts "There is nobody waiting to be served!"
+  elsif deli_line.length > 0
+    puts "Currently serving #{0}."
+    deli_line.shift
+  end
 end

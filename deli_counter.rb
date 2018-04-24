15788ea2b5820b3fb47494c9fae2b903bcf7ff90
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-24 20:51:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(deli_line)
     deli_line
     #puts "The line is currently:"
     deli_line.each_with_index do |num, idx|
-      idx += 1
+      #idx += 1
        puts "The line is currently: #{idx}. #{num}"
     end
   end

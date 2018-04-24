b6b0054dfb92bd7cb6245ff1c199c559f19bfc37
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-24 19:52:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,12 @@
-def line
-
+def line(deli_line, name)
+  if deli_line.length < 1
+    puts "The line is currently empty."
+  elsif deli_line.length > 0
+      return deli_line
+  end
 end
 
-def take_a_number(line, name = "")
+def take_a_number(deli_line, name)
 
 end
 

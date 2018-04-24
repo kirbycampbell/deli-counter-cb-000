f5bd62ce919b9ae9c1029b1e67dce46a09c1db99
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-24 21:04:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,6 @@ def line(deli_line)
   if deli_line.length < 1
     puts "The line is currently empty."
   else
-    #deli_line
     deli_line.each_with_index do |name, num|
       num += 1
       persons_array.push("#{num}. #{name}")

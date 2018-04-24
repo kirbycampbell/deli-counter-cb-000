911af81d18ba366e5fcc3bc4e3d5b20b1366986b
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-24 20:58:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,15 @@
 def line(deli_line)
   if deli_line.length < 1
     puts "The line is currently empty."
-  elsif deli_line.length > 0
+  else # deli_line.length > 0
     #deli_line
     #puts "The line is currently:"
-    deli_line.each_with_index do |name, num|
-      idx += 1
+    deli_line.each_with_index(1) do |name, num|
+      #idx += 1
       persons_array.push("#{num}. #{name}")
       end
        puts "The line is currently: #{persons_array.join(" ")}"
   end
-end
 end
 
 def take_a_number(deli_line, name)

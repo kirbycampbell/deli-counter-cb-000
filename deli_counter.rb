0861d8b0095f1f9490c79c45709e7e43fce24a76
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-24 20:58:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(deli_line)
   if deli_line.length < 1
     puts "The line is currently empty."
   else # deli_line.length > 0
-    #deli_line
+    deli_line
     #puts "The line is currently:"
     deli_line.each_with_index(1) do |name, num|
       #idx += 1
